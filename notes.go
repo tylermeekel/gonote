63a4e32a55850a1617a28fd779d983b546277e06
Tyler Meekel <ty.meekel@gmail.com>
2023-10-01 20:16:42 -0600

    adds support for creating new posts

--- notes.go
+++ notes.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -25,52 +24,69 @@ func (app App) noteRouter() http.Handler {
 	router := chi.NewRouter()
 	router.Get("/", app.handleGetAllNotes)
 	router.Get("/{id}", app.handleGetNoteByID)
+	router.Post("/", app.handlePostNote)
 
 	return router
 }
 
-// queryAllNotes gets all notes from the db connection and returns them as a list of notes
-func (app App) queryAllNotes() []Note {
+// getAllNotes gets all notes from the db connection and returns them as a list of notes
+func (app App) getAllNotes() []Note {
 	var notes []Note
-	rows, err := app.db.Query("SELECT title, content FROM notes;")
+	rows, err := app.db.Query("SELECT * FROM notes")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	for rows.Next() {
 		var note Note
-		rows.Scan(&note.Title, &note.Content)
+		rows.Scan(&note.ID, &note.UserID, &note.Title, &note.Content, &note.CreatedAt)
 		notes = append(notes, note)
 	}
 
 	return notes
 }
 
-// queryOneNoteByID takes an id as an argument and queries the database connection for a Note matching that id, returning a Note object
-func (app App) queryOneNoteByID(id int) Note {
+// getNoteByID takes an id as an argument and queries the db connection for a Note matching that id
+// and then returns a Note object
+func (app App) getNoteByID(id int) Note {
 	var note Note
-	query := fmt.Sprintf("SELECT title, content FROM notes WHERE id = %d;", id)
-	row, err := app.db.Query(query)
+	row, err := app.db.Query("SELECT * FROM notes WHERE id = $1", id)
 	if err != nil {
-		fmt.Println("query error: " + query)
 		fmt.Println(err.Error())
 	}
 	row.Next()
-	row.Scan(&note.Title, &note.Content)
+	err = row.Scan(&note.ID, &note.UserID, &note.Title, &note.Content, &note.CreatedAt)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	return note
 }
 
-// handleGetAllNotes calls the queryAllNotes function and renders the returned notes to the ResponseWriter
-func (app App) handleGetAllNotes(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/components/notes.html")
+// postNote takes a user id, title and content as arguments and inserts a new note into the database
+// and then returns a Note object
+func (app App) postNote(userID int, title, content string) Note {
+	var note Note
+
+	row, err := app.db.Query("INSERT INTO notes(user_id, title, content) VALUES($1, $2, $3) RETURNING *", userID, title, content)
 	if err != nil {
-		w.Write([]byte("server error"))
-		fmt.Println("error parsing file")
+		fmt.Println(err.Error())
+	}
+	if row.Next() {
+		err = row.Scan(&note.ID, &note.UserID, &note.Title, &note.Content, &note.CreatedAt)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
-	notes := app.queryAllNotes()
-	t.Execute(w, notes)
+	return note
+}
 
+//Handlers
+
+// handleGetAllNotes calls the queryAllNotes function and renders the returned notes to the ResponseWriter
+func (app App) handleGetAllNotes(w http.ResponseWriter, r *http.Request) {
+	notes := app.getAllNotes()
+	renderNotes(w, notes)
 }
 
 // handleGetNoteByID calls the queryNoteByID function and renders the returned note to the ResponseWriter
@@ -82,11 +98,31 @@ func (app App) handleGetNoteByID(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	note := app.queryOneNoteByID(id)
-	res, err := json.Marshal(note)
+	//Wrapped in note slice so that template is compatible with any number of notes
+	note := []Note{app.getNoteByID(id)}
+	renderNotes(w, note)
+}
+
+// handlePostNote collects title and content from form and calls the postNote function with them as arguments.
+// It then renders the returned note to the ResponseWriter
+func (app App) handlePostNote(w http.ResponseWriter, r *http.Request) {
+	//TODO: add authentication to collect id
+	userID := 1
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+
+	note := []Note{app.postNote(userID, title, content)}
+	renderNotes(w, note)
+}
+
+//Utilities
+
+func renderNotes(w http.ResponseWriter, notes []Note) {
+	t, err := template.ParseFiles("templates/components/notes.html")
 	if err != nil {
-		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("internal server error"))
 	}
 
-	w.Write(res)
+	t.Execute(w, notes)
 }
